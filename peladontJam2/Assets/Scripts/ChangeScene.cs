b1e5897a4280a9402241f4c6abce742411b5cc1a
DanielZhu26 <dzhu@ucm.es>
2024-03-15 20:47:50 +0100

    cambios del menu de pausa

--- peladontJam2/Assets/Scripts/ChangeScene.cs
+++ peladontJam2/Assets/Scripts/ChangeScene.cs
@@ -9,17 +9,6 @@ public class ChangeScene : MonoBehaviour
 
     [SerializeField] int sceneID;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void LoadLevel(int sceneID)
     {
