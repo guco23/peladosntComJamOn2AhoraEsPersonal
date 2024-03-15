2cd9346f7fbd8c4a4b0e7c4a85aae2f40049cb2f
LuisEnriqueCabelloBlanco <luisecab@ucm.es>
2024-03-15 18:58:28 +0100

    El PIBE DEL SONIDO HA TRABAJADO

--- peladontJam2/Assets/Scripts/SoldierMoveComponent.cs
+++ peladontJam2/Assets/Scripts/SoldierMoveComponent.cs
@@ -1,3 +1,5 @@
+using FMOD.Studio;
+using FMODUnity;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -17,12 +19,27 @@ public class SoldierMoveComponent : MonoBehaviour
     [SerializeField]
     [Tooltip("La velocidad del personaje")]
     float speed;
-
+    public FMODUnity.EventReference inputsound;
+    public float timeBetweenSteps = 0.5f;
     // Start is called before the first frame update
     void Start()
     {
         rb = this.GetComponent<Rigidbody>();
+
         continueMoving();
+
+    }
+    private void Update()
+    {
+        if(timeBetweenSteps < 0 && rb.velocity.magnitude >0)
+        {
+            timeBetweenSteps = 0.5f;
+            EventInstance soundInstance = RuntimeManager.CreateInstance(inputsound.Path);
+            soundInstance.start();
+            soundInstance.release();
+
+        }
+        timeBetweenSteps -=Time.deltaTime;
     }
 
     public void stopMoving() {
@@ -31,6 +48,7 @@ public class SoldierMoveComponent : MonoBehaviour
 
     public void continueMoving() {
         rb.velocity = transform.forward * speed;
+        
     }
     public EstadoSoldado GetEstadoSoldado() {
         return estado;
