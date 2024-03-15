db491d9c68d96f435586039bcc19c569640855ff
Adelpozo04 <addelp01@ucm.es>
2024-03-15 16:10:53 +0100

    Camara move y cosillas

---
+++ peladontJam2/Assets/Scripts/ResourseManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class ResourseManager : MonoBehaviour
+{
+
+
+    [SerializeField] private float cooldown;
+
+    [SerializeField] private float resourseWin;
+
+    private float elapsedTime;
+    
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
