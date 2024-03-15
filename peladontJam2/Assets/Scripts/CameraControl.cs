b84d63a40a760fefb7df0a408504f30c5c8deb45
Adelpozo04 <addelp01@ucm.es>
2024-03-15 15:57:28 +0100

    Camara con movimiento

--- peladontJam2/Assets/Scripts/CameraControl.cs
+++ peladontJam2/Assets/Scripts/CameraControl.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.XR;
 
 public class CameraControl : MonoBehaviour
 {
@@ -11,26 +13,34 @@ public class CameraControl : MonoBehaviour
 
     #endregion
 
+    private Vector3 move;
+    private Vector2 myDir;
+    private CharacterController controller;
+    private Camera mainCamera;
+
     #region methods
 
-    public void MoveCamera(bool dir)
+
+    public void DirMovement(InputAction.CallbackContext context)
     {
 
-        int moveDir = 0;
-
-        if (dir)
-        {
-            moveDir = 1;
-        }
-        else
-        {
-            moveDir = -1;
-        }
-
-        
+        myDir = context.ReadValue<Vector2>();
 
     }
-
     #endregion
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        mainCamera = Camera.main;
+    }
+
+    void Update()
+    {
+        move = (myDir.y * mainCamera.transform.forward + myDir.x * mainCamera.transform.right) * speed_;
+
+        controller.Move(move * Time.deltaTime);
+
+    }
 
 }
