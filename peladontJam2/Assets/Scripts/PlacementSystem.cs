e33da08947b75a813ed16a6eb82f42241f88ddc2
samuel <samuga05@ucm.es>
2024-03-15 16:09:09 +0100

    spawneo de soldados

--- peladontJam2/Assets/Scripts/PlacementSystem.cs
+++ peladontJam2/Assets/Scripts/PlacementSystem.cs
@@ -5,11 +5,18 @@ using UnityEngine;
 public class PlacementSystem : MonoBehaviour
 {
 
+    [SerializeField]
+    private Vector3 placeOffSet = new Vector3(1, 1, 1);
+
+    [SerializeField]
+    private Vector3 placeOffSetSpawn = new Vector3(1, 1, 1);
     #region references
 
-    [SerializeField] private GameObject mouseIcon, cellIndicator;
+    [SerializeField] private GameObject mouseIcon, cellIndicator,basicSoldierPrefab;
     [SerializeField] private InputManager inputManager;
     [SerializeField] private Grid grid_;
+
+    [SerializeField] int spawnCellX;
 
     #endregion
 
@@ -20,7 +27,26 @@ public class PlacementSystem : MonoBehaviour
     {
         Vector3 mousePos = inputManager.GetSelectedMapPoint();
         Vector3Int cellPos = grid_.WorldToCell(mousePos);
-        mouseIcon.transform.position = mousePos;
+        print(cellPos);
+        mouseIcon.transform.position = grid_.CellToWorld(cellPos) +placeOffSet;
         cellIndicator.transform.position = grid_.CellToWorld(cellPos);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            SpawnBasicSoldier();
+        }
+    }
+
+    public void SpawnBasicSoldier()
+    {
+        Vector3 mousePos = inputManager.GetSelectedMapPoint();
+        Vector3Int cellPos = grid_.WorldToCell(mousePos);
+
+        cellPos.x = spawnCellX;
+
+        GameObject soldier = Instantiate(basicSoldierPrefab, grid_.CellToWorld(cellPos) + placeOffSetSpawn, Quaternion.identity);
+
+        soldier.transform.Rotate(new Vector3(0, 90, 0));
+    
     }
 }
