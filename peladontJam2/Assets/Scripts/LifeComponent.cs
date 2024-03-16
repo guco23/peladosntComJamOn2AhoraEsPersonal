07c7a1a195a3754a8bce6100515b5dc2390e5fad
David-104 <dcanelad@ucm.es>
2024-03-16 19:36:08 +0100

    conflicto en samuscene

--- peladontJam2/Assets/Scripts/LifeComponent.cs
+++ peladontJam2/Assets/Scripts/LifeComponent.cs
@@ -31,13 +31,16 @@ public class LifeComponent : MonoBehaviour
         life -= damage;
         EventInstance soundInstance = RuntimeManager.CreateInstance(inputsound.Path);
 
-        if (bloodEffect) bloodEffect.SendEvent("Bleed");
-
         //si tenemos 0 o menos vida, destroy
         if (life <= 0) {
             soundInstance.setParameterByName("Alive",1f);
             soundInstance.start();
             soundInstance.release();
+            if (bloodEffect)
+            {
+                bloodEffect.SendEvent("Bleed");
+                bloodEffect.gameObject.transform.parent = null;
+            }
             Destroy(gameObject);
             return true;
         }
