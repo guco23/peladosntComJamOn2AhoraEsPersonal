bdc4a0ee74a7bb6b17737199e961f5f8c7f0dd68
LuisEnriqueCabelloBlanco <luisecab@ucm.es>
2024-03-16 23:45:53 +0100

    Creados eventos de victoria y derrota (sonido) y enlazado recursos con la UI

--- peladontJam2/Assets/Scripts/showresources.cs
+++ peladontJam2/Assets/Scripts/showresources.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class showresources : MonoBehaviour
 {
@@ -11,9 +12,13 @@ public class showresources : MonoBehaviour
     [SerializeField]
     TMP_Text textMeshPro;
 
+    [SerializeField]
+    Slider resourcesSlider;
+
     private void Update()
     {
         textMeshPro.text = "Resources: " + playerResources.getResources();
+        resourcesSlider.value = playerResources.getResources();
     }
 
 
