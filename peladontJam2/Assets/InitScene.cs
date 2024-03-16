82b1fd7b8d926aae0a4fdae75ae9ccbe443d1a29
Vivu24 <javivu2.12004@gmail.com>
2024-03-16 20:03:46 +0100

    Me retiro

---
+++ peladontJam2/Assets/InitScene.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InitScene : MonoBehaviour
+{
+    [SerializeField] private Animator _animator;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fadeIn();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void fadeIn()
+    {
+        _animator.SetTrigger("FadeIn");
+    }
+}
