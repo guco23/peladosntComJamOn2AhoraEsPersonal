366679e41d66096a01bf50bdf4dddf12e39877de
Adelpozo04 <addelp01@ucm.es>
2024-03-17 14:41:33 +0100

    Puede que tutorial acabado

--- peladontJam2/Assets/Scripts/TutorialManager.cs
+++ peladontJam2/Assets/Scripts/TutorialManager.cs
@@ -11,6 +11,12 @@ public class TutorialManager : MonoBehaviour
 
     [SerializeField] private GameObject canvas_;
 
+    [SerializeField] private GameObject tropa_;
+
+    [SerializeField] private GameObject trincheras_;
+
+    [SerializeField] private GameObject menuTrinchera_;
+
     [SerializeField] private GameObject [] texts_;
 
     private int currentText_;
@@ -22,6 +28,49 @@ public class TutorialManager : MonoBehaviour
         texts_[currentText_].SetActive(true);
 
         currentText_++;
+
+    }
+
+    public void CompleteCondition()
+    {
+
+        //mover camara
+        if(currentText_ == 1)
+        {
+
+            
+
+        }
+        //info niebla
+        else if(currentText_ == 2)
+        {
+
+            
+
+        }
+        //colocar soldado
+        else if(currentText_ == 3)
+        {
+
+            resoursesSystem_.SetActive(true);
+            canvas_.SetActive(true);
+            trincheras_.SetActive(false);
+            menuTrinchera_.SetActive(false);
+
+        }
+        //colocar zafador
+        else if(currentText_ == 4)
+        {
+            trincheras_.SetActive(true);
+
+        }
+        //meter soldados en trincheras
+        else if(currentText_ == 5)
+        {
+
+        }
+
+        NextPartText();
 
     }
 
@@ -38,13 +87,19 @@ public class TutorialManager : MonoBehaviour
             texts_[i].SetActive(false);
         }
 
-
+        NextPartText();
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            CompleteCondition();
+        }
+
+
     }
 }
